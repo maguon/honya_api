63f10a8d2ce7875f991439ebf4196ed298e45550
zwl123 <wenlong.zhang@myxxjs.com>
2018-03-23 09:59:56 +0800

    增加仓储车辆品牌统计

--- server.js
+++ server.js
@@ -198,6 +198,7 @@ function createServer() {
      * storageParking Module
      */
     server.get('/api/storageParking',storageParking.queryStorageParking);
+    server.get('/api/storage/:storageId/makeStat',storageParking.queryStorageParkingMakeStat);
     server.put({path:'/api/user/:userId/storageParking/:parkingId',contentType: 'application/json'} ,storageParking.updateStorageParking,sysRecord.saveCarRecord);
 
     /**
