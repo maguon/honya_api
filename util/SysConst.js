309a0ffcc27efb64309559fdc37f5854272a4272
zwl123 <wenlong.zhang@myxxjs.com>
2018-04-08 10:27:09 +0800

    创建SysConst.js

---
+++ util/SysConst.js
@@ -0,0 +1,14 @@
+/**
+ * Created by zwl on 2018/4/8.
+ */
+
+var USER_TYPE  ={
+    admin : 99,  //系统管理员
+    storage_op : 21, //仓储部操作员
+    storage_admin : 29//仓储部管理员
+};
+
+
+module.exports = {
+    USER_TYPE : USER_TYPE
+}
