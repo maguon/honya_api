520ebafc45db0f4e3c8ec86dcdb3109db977c10e
zwl123 <wenlong.zhang@myxxjs.com>
2018-04-11 15:33:12 +0800

    创建country_info表

--- dao/PortDAO.js
+++ dao/PortDAO.js
@@ -7,10 +7,10 @@ var serverLogger = require('../util/ServerLogger.js');
 var logger = serverLogger.createLogger('PortDAO.js');
 
 function addPort(params,callback){
-    var query = " insert into port_info (port_name,country_name,address,remark) values ( ? , ? , ? , ? )";
+    var query = " insert into port_info (port_name,country_id,address,remark) values ( ? , ? , ? , ? )";
     var paramsArray=[],i=0;
     paramsArray[i++]=params.portName;
-    paramsArray[i++]=params.countryName;
+    paramsArray[i++]=params.countryId;
     paramsArray[i++]=params.address;
     paramsArray[i]=params.remark;
     db.dbQuery(query,paramsArray,function(error,rows){
@@ -20,19 +20,21 @@ function addPort(params,callback){
 }
 
 function getPort(params,callback) {
-    var query = " select * from port_info where id is not null ";
+    var query = " select p.* from port_info p " +
+        " left join country_info c on p.country_id = c.id " +
+        " where p.id is not null ";
     var paramsArray=[],i=0;
     if(params.portId){
         paramsArray[i++] = params.portId;
-        query = query + " and id = ? ";
+        query = query + " and p.id = ? ";
     }
     if(params.portName){
         paramsArray[i++] = params.portName;
-        query = query + " and port_name = ? ";
+        query = query + " and p.port_name = ? ";
     }
-    if(params.countryName){
-        paramsArray[i++] = params.countryName;
-        query = query + " and country_name = ? ";
+    if(params.countryId){
+        paramsArray[i++] = params.countryId;
+        query = query + " and p.country_id = ? ";
     }
     if (params.start && params.size) {
         paramsArray[i++] = parseInt(params.start);
@@ -46,10 +48,10 @@ function getPort(params,callback) {
 }
 
 function updatePort(params,callback){
-    var query = " update port_info set port_name = ? , country_name = ? , address = ?, remark = ? where id = ? " ;
+    var query = " update port_info set port_name = ? , country_id = ? , address = ?, remark = ? where id = ? " ;
     var paramsArray=[],i=0;
     paramsArray[i++]=params.portName;
-    paramsArray[i++]=params.countryName;
+    paramsArray[i++]=params.countryId;
     paramsArray[i++]=params.address;
     paramsArray[i++]=params.remark;
     paramsArray[i]=params.portId;
--- db/sql/0.0.3/update_base.sql
+++ db/sql/0.0.3/update_base.sql
@@ -4,10 +4,32 @@
 DROP TABLE IF EXISTS `port_info`;
 CREATE TABLE `port_info` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `port_name` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '港口名称',
-  `country_name` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '国家',
+  `port_name` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '港口名称',
+  `country_id` int(10) DEFAULT NULL COMMENT '国家ID',
   `address` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '详细地址',
   `remark` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '备注',
+  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- ----------------------------
+-- Table structure for ship_company_info
+-- ----------------------------
+DROP TABLE IF EXISTS `ship_company_info`;
+CREATE TABLE `ship_company_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ship_company_name` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '船务公司名称',
+  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- ----------------------------
+-- Table structure for country_info
+-- ----------------------------
+DROP TABLE IF EXISTS `country_info`;
+CREATE TABLE `country_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `country_name` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '国家ID',
   `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `updated_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`)
