19d44ce99a8febb61910cb5f27fa4ff645c254e0
zwl123 <wenlong.zhang@myxxjs.com>
2018-04-24 15:44:31 +0800

    海运管理

--- dao/ShipTransDAO.js
+++ dao/ShipTransDAO.js
@@ -30,8 +30,10 @@ function addShipTrans(params,callback){
 }
 
 function getShipTrans(params,callback) {
-    var query = " select st.*,sum(sto.ship_trans_fee) as ship_trans_fee from ship_trans_info st" +
+    var query = " select st.*,u.real_name as start_ship_user_name,sum(sto.ship_trans_fee) as ship_trans_fee " +
+        " from ship_trans_info st " +
         " left join ship_trans_order sto on st.id = sto.ship_trans_id " +
+        " left join user_info u on st.start_ship_user_id = u.uid " +
         " where st.id is not null ";
     var paramsArray=[],i=0;
     if(params.shipTransId){
