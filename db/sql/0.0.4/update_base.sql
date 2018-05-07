17ea4f4db3130cf3d879b6d424988cc362376bfc
zwl123 <wenlong.zhang@myxxjs.com>
2018-05-07 10:18:01 +0800

    更新车采购类型

--- db/sql/0.0.4/update_base.sql
+++ db/sql/0.0.4/update_base.sql
@@ -2,4 +2,4 @@
 -- 2018-05-07 更新
 -- ----------------------------
 ALTER TABLE `car_info`
-ADD COLUMN `purchase_type`  tinyint(1) NULL DEFAULT 0 COMMENT '商品车采购类型(1-仓储车,2-金融车)' AFTER `mso_status`;
+ADD COLUMN `purchase_type`  tinyint(1) NULL DEFAULT 0 COMMENT '商品车采购类型(0-仓储车,1-金融车)' AFTER `mso_status`;
