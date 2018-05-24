4366b5116f3568427045d1cb8e7251bf6d4af0bb
zwl123 <wenlong.zhang@myxxjs.com>
2018-05-24 14:11:57 +0800

    还款增加查询参数

--- dao/LoanRepaymentDAO.js
+++ dao/LoanRepaymentDAO.js
@@ -25,16 +25,47 @@ function addLoanRepayment(params,callback){
 }
 
 function getLoanRepayment(params,callback) {
-    var query = " select lr.* from loan_repayment lr where lr.id is not null ";
+    var query = " select lr.*,e.entrust_type,e.short_name from loan_repayment lr " +
+        " left join loan_info l on lr.loan_id = l.id " +
+        " left join entrust_info e on l.entrust_id = e.id " +
+        " left join loan_rep_credit_rel lrcr on lr.id = lrcr.repayment_id " +
+        " left join loan_rep_payment_rel lrpr on lr.id = lrpr.repayment_id " +
+        " where lr.id is not null ";
     var paramsArray=[],i=0;
     if(params.repaymentId){
         paramsArray[i++] = params.repaymentId;
         query = query + " and lr.id = ? ";
     }
+    if(params.entrustId){
+        paramsArray[i++] = params.entrustId;
+        query = query + " and l.entrust_id = ? ";
+    }
     if(params.loanId){
         paramsArray[i++] = params.loanId;
         query = query + " and lr.loan_id = ? ";
     }
+    if(params.repaymentStatus){
+        paramsArray[i++] = params.repaymentStatus;
+        query = query + " and lr.repayment_status = ? ";
+    }
+    if(params.creditId){
+        paramsArray[i++] = params.creditId;
+        query = query + " and lrcr.credit_id = ? ";
+    }
+    if(params.paymentId){
+        paramsArray[i++] = params.paymentId;
+        query = query + " and lrpr.payment_id = ? ";
+    }
+    if(params.createdOnStart){
+        paramsArray[i++] = params.createdOnStart +" 00:00:00";
+        query = query + " and lr.created_on >= ? ";
+    }
+    if(params.createdOnEnd){
+        paramsArray[i++] = params.createdOnEnd +" 23:59:59";
+        query = query + " and lr.created_on <= ? ";
+    }
+    query = query + " group by lr.id ";
+    query = query + " order by lr.id desc ";
     if (params.start && params.size) {
         paramsArray[i++] = parseInt(params.start);
         paramsArray[i++] = parseInt(params.size);
