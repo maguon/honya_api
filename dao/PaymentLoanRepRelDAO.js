84a30e4018292340b9be8a644af5219e726d27b9
zwl123 <wenlong.zhang@myxxjs.com>
2018-07-16 11:32:21 +0800

    支付管理查询关联还款

--- dao/PaymentLoanRepRelDAO.js
+++ dao/PaymentLoanRepRelDAO.js
@@ -18,14 +18,21 @@ function addPaymentLoanRepRel(params,callback){
 }
 
 function getPaymentLoanRepRel(params,callback) {
-    var query = " select plrr.*,p.payment_type,p.number,p.payment_money,p.created_on as created_payment_date " +
+    var query = " select plrr.*,l.id as loan_id,l.loan_money,lr.repayment_money,lr.interest_money,lr.fee,lr.created_on as repayment_date " +
+        ",p.payment_type,p.number,p.payment_money,p.created_on as created_payment_date  " +
         " from payment_loan_rep_rel plrr " +
         " left join payment_info p on plrr.payment_id = p.id " +
+        " left join loan_repayment lr on plrr.repayment_id = lr.id " +
+        " left join loan_info l on lr.loan_id = l.id " +
         " where plrr.id is not null ";
     var paramsArray=[],i=0;
     if(params.repaymentId){
         paramsArray[i++] = params.repaymentId;
         query = query + " and plrr.repayment_id = ? ";
+    }
+    if(params.paymentId){
+        paramsArray[i++] = params.paymentId;
+        query = query + " and plrr.payment_id = ? ";
     }
     if (params.start && params.size) {
         paramsArray[i++] = parseInt(params.start);
